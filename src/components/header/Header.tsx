aed7d188eb73844cd923f0bf9f15e9015e13f133
Marc Schulmeister <marcschulm@gmail.com>
2022-06-13 17:52:26 -0700

    change dictionary -> directory

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -2,7 +2,7 @@ export default function header() {
   return (
     <>
       <h1 className="text-4xl sm:text-7xl border-b border-slate-400 mb-10 text-slate-600 pb-2">
-        User Dictionary
+        User Directory
       </h1>
     </>
   );
